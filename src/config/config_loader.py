a00dc4b5f6d7d8b735e168f73a21979f05cf2db1
zTeylar <leonardodashi09@gmail.com>
2026-07-03 17:11:22 +0100

    refactor: added metadata to PipelineConfig

--- src/config/config_loader.py
+++ src/config/config_loader.py
@@ -24,6 +24,7 @@ class PipelineConfig:
     stft: STFTConfig
     hdf5_file: Path
     manifest_file: Path
+    metadata_file: Path
 
 
 def load_config(config_path: Path = DEFAULT_CONFIG_PATH, dataset_name: str = None) -> PipelineConfig:
@@ -33,14 +34,15 @@ def load_config(config_path: Path = DEFAULT_CONFIG_PATH, dataset_name: str = Non
     dataset_dict = config_dict['datasets'][dataset_name]
 
     dataset_config = DatasetConfig(
-        data_dir=Path(PROJECT_ROOT / dataset_dict['data_dir']),
+        data_dir=PROJECT_ROOT / dataset_dict['data_dir'],
         num_channels=dataset_dict['num_channels'],
         n_samples=dataset_dict['n_samples']
     )
     stft_config = STFTConfig(**config_dict['stft_params'])
-    processed_dir = Path(PROJECT_ROOT / config_dict['storage']['processed_dir_path'])
+    processed_dir = PROJECT_ROOT / config_dict['storage']['processed_dir_path']
     processed_dir.mkdir(parents=True, exist_ok=True)
     hdf5_file = processed_dir / f"{dataset_name}_data.h5"
     manifest_file = processed_dir / f"{dataset_name}_manifest.json"
+    metadata_file = processed_dir / f"{dataset_name}_metadata.json"
 
-    return PipelineConfig(dataset=dataset_config, stft=stft_config, hdf5_file=hdf5_file, manifest_file=manifest_file)
+    return PipelineConfig(dataset=dataset_config, stft=stft_config, hdf5_file=hdf5_file, manifest_file=manifest_file, metadata_file=metadata_file)
